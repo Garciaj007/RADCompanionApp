2fd3acac3f2bb3307995b1309c332fc4447a7781
Juriel Garcia <jurielgarcia2010@gmail.com>
2020-01-30 15:38:29 -0500

    Added Initial Project Setup
    Generated Project for Single App View. No Core Data, No Unit Tests, No UI Tests.

---
+++ RADCompanionApp/RADCompanionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RADCompanionApp
+//
+//  Created by Beyond Help on 2020-01-30.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
